7f93ab289cfe6e9959e2766d771d108b488c105a
YuriiDDD <yurii_daniuk@jabil.com>
2021-12-02 12:05:17 +0200

    Added Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // получаем текущий поток
+            Thread t = Thread.CurrentThread;
+
+            //получаем имя потока
+            Console.WriteLine($"Имя потока: {t.Name}");
+            t.Name = "Метод Main";
+            Console.WriteLine($"Имя потока: {t.Name}");
+
+            Console.WriteLine($"Запущен ли поток: {t.IsAlive}");
+            Console.WriteLine($"Приоритет потока: {t.Priority}");
+            Console.WriteLine($"Статус потока: {t.ThreadState}");
+
+            // получаем домен приложения
+            Console.WriteLine($"Домен приложения: {Thread.GetDomain().FriendlyName}");
+
+            Console.ReadLine();
+        }
+    }
+}
